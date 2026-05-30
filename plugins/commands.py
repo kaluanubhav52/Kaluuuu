558c96d36bb2e2527a74af56bda607a04f1bcb86
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-30 21:53:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,18 +21,6 @@ from utils import verify_user, check_token, check_verification, get_token
 from config import *
 from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 
-
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-        except Exception as e:
-            pass
-    return btn
     
 # Logging configurations
 logger = logging.getLogger(__name__)
@@ -123,20 +111,10 @@ async def start(client, message):
         try: await message.react(emoji="⚡️", big=True)
         except Exception: pass
 
-    if AUTH_CHANNEL:
-        try:
-            btn = await is_subscribed(client, message, AUTH_CHANNEL)
-            if btn:
-                username = (await client.get_me()).username
-                if message.command[1]:
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
-                else:
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
-                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
-                return
-        except Exception as e:
-            print(e)
-            
+    if not await is_subscribed(client, user_id):
+        return await not_joined(client, message)
+
+    
     username = client.me.username
     user_id = message.from_user.id
     
